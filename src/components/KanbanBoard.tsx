7f78ed53279245971478d55b8d70ef9bb1caf9a1
Saban-94 <hsaban2025@gmail.com>
2026-06-02 04:06:01 +0300

    Update KanbanBoard.tsx

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -3,6 +3,7 @@ import { motion } from 'motion/react';
 import { DragDropContext, Droppable, Draggable, DropResult } from '@hello-pangea/dnd';
 import { Order, Driver } from '../types';
 import { OrderCard } from './OrderCard';
+import { OrderCard } from './OrderForm';
 import { 
   Clock, 
   Truck, 
